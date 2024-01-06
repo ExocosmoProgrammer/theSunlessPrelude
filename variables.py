c6cf4c8592266986dbc4a50ff4c9c7a2b9dc79d0
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-01-06 05:25:35 -0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,4 +5,4 @@ foesPerLevel = {1: ['alien colonist', 'alien bodyguard', 'alien police', 'alien 
                 3: ['alien combat scientist', 'alien assassin', 'alien minion'],
                 4: ['alien priest', 'alien gangster', 'alien cultist', 'alien nun']}
 bossesPerLevel = {1: 'alien commander', 2: 'alien pilot', 3: 'alien warrior'}
-oneTimeUseItems = ['solution', 'stun grenade', 'throwing knife', 'vial of poison']
+oneTimeUseItems = ['solution', 'stun grenade', 'throwing knife', 'vial of poison', 'serrated knife']
